e9bef164ddae3a09ab2cd3715b14987e404b32e1
Lilyach <lilichf85@gmail.com>
2024-02-06 15:33:39 +0500

    Lesson_13_mts

--- src/test/java/pages/ResultPage.java
+++ src/test/java/pages/ResultPage.java
@@ -1,7 +1,9 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -10,18 +12,21 @@ import java.time.Duration;
 
 public class ResultPage extends BasePage {
 
-    private final By paymentFrameLocator = By.xpath("//iframe[@class='bepaid-iframe']");
-    private final By paymentWindowLocator = By.xpath("//div[@class='app-wrapper__content']");
+    @FindBy(xpath = "//iframe[@class='bepaid-iframe']")
+    private WebElement paymentFrame;
+    @FindBy(xpath = "//div[@class='app-wrapper__content']")
+    private WebElement paymentWindow;
 
     public ResultPage(WebDriver driver) {
         super(driver);
+        PageFactory.initElements(driver, this);
     }
 
     public boolean isPaymentWindowDisplayed() {
         WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
 
-        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(paymentFrameLocator));
-        boolean isDisplayed = wait.until(ExpectedConditions.visibilityOfElementLocated(paymentWindowLocator)).isDisplayed();
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(paymentFrame));
+        boolean isDisplayed = wait.until(ExpectedConditions.visibilityOf(paymentWindow)).isDisplayed();
 
         getDriver().switchTo().defaultContent();
         return isDisplayed;
