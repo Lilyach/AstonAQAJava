4cd5e9489bfda8f2f5e252fee757a5a84bca4f0e
Lilyach <lilichf85@gmail.com>
2024-02-20 01:28:39 +0500

    Lesson_18

--- src/main/java/homework/Main.java
+++ src/main/java/homework/Main.java
@@ -3,7 +3,6 @@ package homework;
 
 public class Main {
     public static void main(String[] args) {
-        FactorialCalculator calculator = new FactorialCalculator();
-        System.out.println(calculator.calculateFactorial(5));
+
     }
 }
